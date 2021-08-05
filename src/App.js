213e3b521ba61ac9dbca78be472f20236eba85b1
Abdul <hannan.abdul007@gmail.com>
2021-08-05 12:11:49 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Pagination from './component/pagination';
+
 
 function App() {
-  return (
+  const [user,setUser]=useState([]);
+  const [showPerPage,setShowPerPage] = useState(1);
+  const [pagination,setPagination] = useState({
+    start:0,end:showPerPage
+  })
+
+  const getData = async() => {
+      const response = await axios.get(" https://reqres.in/api/users?page=2");
+      
+      // console.log(response.data.data);
+      setUser(response.data.data);
+
+  }
+
+  useEffect(()=>{
+      getData();
+  },[])
+
+  const onPaginationChange = (start,end) =>{
+
+    setPagination({start:start,end:end});
+
+  }
+  return(
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-4">
+      <div className="row">
+        {user.slice(pagination.start,pagination.end).map((post) => (
+          <div className="col-lg-12 mb-3" key={post.id}>
+            <div className="card">
+              <div className="card-body">
+                <h4>
+                 {post.id} {post.first_name} 
+                </h4>
+                <img src={post.avatar} alt="imgage"/>
+                <h5> Email: {post.email}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+     <Pagination showPerPage={showPerPage} onPaginationChange={onPaginationChange} total={user.length} />
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
